54c0ea4dff48cbed98f3502c587e058e8d887028
Han Verstraete <welteki@pm.me>
2021-07-08 15:14:51 +0200

    Build notes using nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
   description = "Personal wiki and notes";
   
+  inputs.utils.url   = "github:numtide/flake-utils";
   inputs.neuron.url = "github:srid/neuron";
 
-  outputs = { self, nixpkgs, neuron }: 
+  outputs = { self, nixpkgs, utils, neuron }: 
     let
       supportedSystems = [
         "x86_64-linux"
@@ -13,6 +14,21 @@
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
     {
+      overlay = final: prev: {
+        notes = prev.stdenv.mkDerivation {
+          pname = "welteki-notes";
+          version = "0.0.1";
+          src = ./.;
+          buildInputs = [ prev.neuron ];
+          buildPhase = ''
+            neuron gen --pretty-urls
+          '';
+          installPhase = ''
+            cp -r .neuron/output/ $out
+          '';
+        };
+      };
+
       devShell = forAllSystems (system:
         let
           inherit (nixpkgs.legacyPackages.${system}) mkShell;
@@ -22,5 +38,19 @@
             neuron.defaultPackage.${system}
           ];
         });
-    };
+    } // utils.lib.eachDefaultSystem (system:
+    let
+      neuron-overlay = final: prev: {
+        neuron = neuron.defaultPackage.${system};
+      };
+
+      pkgs = import nixpkgs {
+        inherit system;
+        # important: neuron-overlay should be applied first
+        overlays = [ neuron-overlay self.overlay ];
+      };
+    in
+    {
+      defaultPackage = pkgs.notes;
+    });
 }
